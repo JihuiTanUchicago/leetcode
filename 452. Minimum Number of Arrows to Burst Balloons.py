3ed42b5499be29aa4e50f661dba29127d69c8791
Todd Tan <toddtan@Todds-Laptop.local>
2024-07-07 11:18:38 -0500

    714

--- 452. Minimum Number of Arrows to Burst Balloons.py
+++ 452. Minimum Number of Arrows to Burst Balloons.py
@@ -1,13 +1,10 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        #balloons with overlapping points could be shot at the same time
-        start = 0
-        points = sorted(points, key=lambda x:x[1])
-        count = 1
-        for i in range(1,len(points)):
-            if points[i][0] <= points[start][1]:
-                continue
-            else:
+        # Sort by end point and maintain a pointer to the latest start point
+        points.sort(key=lambda x: x[1])
+        count, start = 1, 0
+        for i in range(1, len(points)):
+            if points[start][1] < points[i][0]:
                 count += 1
                 start = i
         return count
