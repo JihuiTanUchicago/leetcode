563e1703995f827e4b3d9e5326708bd75641802d
JihuiTanUchicago <tanjihui@uchicago.edu>
2025-01-26 12:34:57 -0800

    1930

---
+++ DSA/python/1930. Unique Length-3 Palindromic Subsequences.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        count = 0
+        record_set = set()
+        record = defaultdict(set)
+        for i in range(len(s)):
+            if s[i] in record_set:
+                continue
+            for j in range(len(s)-1, i+1, -1):
+                if s[i] == s[j] and s[i] not in record_set:
+                    count += len(set(s[i+1:j]))
+                    record_set.add(s[i])
+                    break
+        return count
+
+
+
