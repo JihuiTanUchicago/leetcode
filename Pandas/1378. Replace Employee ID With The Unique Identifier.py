e7b7ef7ea3ad2519e9fc678e50bf62f8f1c84261
JihuiTanUchicago <tanjihui@uchicago.edu>
2025-03-22 20:07:37 -0700

    1378

---
+++ Pandas/1378. Replace Employee ID With The Unique Identifier.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    df = employees.merge(employee_uni, on="id", how="left")
+    return df[['unique_id', 'name']]
+    
