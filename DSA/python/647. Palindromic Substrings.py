c5c932202d8788a63b5555f01c187ea4de9073f9
JihuiTanUchicago <tanjihui@uchicago.edu>
2025-08-30 22:12:29 -0700

    647

---
+++ DSA/python/647. Palindromic Substrings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                string = s[i:j+1]
+                if string == string[::-1]:
+                    count += 1
+        return count
