42a1b86de0acc6af0cb066eea5022806f9c97062
JihuiTanUchicago <tanjihui@uchicago.edu>
2025-09-29 19:54:06 -0700

    596

---
+++ DSA/python/596. Classes With at Least 5 Students.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    df = courses.groupby('class').size().reset_index(name='count')
+    return df[df['count'] >= 5][['class']]
