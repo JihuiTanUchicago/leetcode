2dc1b77338d8df799f63d57f71590db3bc7b6bfb
JihuiTanUchicago <tanjihui@uchicago.edu>
2025-09-29 20:17:30 -0700

    1693

---
+++ DSA/python/1693. Daily Leads and Partners.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    df = daily_sales.groupby(['date_id', 'make_name']).agg({
+        'lead_id':'nunique',
+        'partner_id':'nunique'
+    }).reset_index()
+
+    df = df.rename(columns={
+        'lead_id': 'unique_leads',
+        'partner_id': 'unique_partners'
+    })
+
+    return df
