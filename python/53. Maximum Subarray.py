36f4f7f5657e9f4fcec273e3d6ba894ac327ed96
JihuiTanUchicago <tanjihui@uchicago.edu>
2025-10-11 21:16:00 -0700

    53

--- python/53. Maximum Subarray.py
+++ python/53. Maximum Subarray.py
@@ -1,14 +1,27 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = float('-inf')
-        cur_sum = 0
-        for i in range(len(nums)):
-            if nums[i] > nums[i]+cur_sum:
-                cur_sum = nums[i]
-            else:
-                cur_sum += nums[i]
-            max_sum = max(max_sum, cur_sum)
-        return max_sum
+        max_summation = float('-inf')
+        summation = float('-inf')
+        i = 0
+        while i < len(nums):
+            summation = max(nums[i], summation + nums[i])
+            max_summation = max(summation, max_summation)
+            i += 1
+
+        return max_summation
+            
+            
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         max_sum = float('-inf')
+#         cur_sum = 0
+#         for i in range(len(nums)):
+#             if nums[i] > nums[i]+cur_sum:
+#                 cur_sum = nums[i]
+#             else:
+#                 cur_sum += nums[i]
+#             max_sum = max(max_sum, cur_sum)
+#         return max_sum
             
         
         
