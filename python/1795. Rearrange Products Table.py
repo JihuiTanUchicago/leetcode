6d9f67dff9f86f353262104cfdb187de07bbec47
JihuiTanUchicago <tanjihui@uchicago.edu>
2025-10-11 18:42:06 -0700

    1795

---
+++ python/1795. Rearrange Products Table.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    p = products.melt(id_vars=['product_id'], value_vars=['store1', 'store2', 'store3'], var_name='store', value_name='price')
+    return p.dropna()
