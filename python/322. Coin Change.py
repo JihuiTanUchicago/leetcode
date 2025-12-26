2b9951e238e8cdfc2f6506e615c76f71f4fe69a6
JihuiTanUchicago <tanjihui@uchicago.edu>
2025-12-25 23:21:36 -0500

    322

--- python/322. Coin Change.py
+++ python/322. Coin Change.py
@@ -2,12 +2,21 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [float('inf') for i in range(amount+1)]
         dp[0] = 0
-        coins.sort()
-        for i in range(1, amount+1):
-            for coin in coins:
-                if i < coin:
-                    continue
+        for coin in coins:
+            for i in range(coin, amount+1):
                 dp[i] = min(dp[i], dp[i-coin] + 1)
-        if dp[-1] == float('inf'):
-            return -1
-        return dp[-1]
+        
+        return dp[amount] if dp[amount] != float('inf') else -1
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         dp = [float('inf') for i in range(amount+1)]
+#         dp[0] = 0
+#         coins.sort()
+#         for i in range(1, amount+1):
+#             for coin in coins:
+#                 if i < coin:
+#                     continue
+#                 dp[i] = min(dp[i], dp[i-coin] + 1)
+#         if dp[-1] == float('inf'):
+#             return -1
+#         return dp[-1]
