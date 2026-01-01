7a9fa87d2823b2e6e51191f456e8d46988892535
Todd Tan <91858809+JihuiTanUchicago@users.noreply.github.com>
2025-12-31 19:54:59 -0500

    Refactor cycle detection in linked list algorithm

--- python/141. Linked List Cycle.py
+++ python/141. Linked List Cycle.py
@@ -6,18 +6,40 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if head == None:
-            return False
         turtle = head
-        rabbit = head
-        while True:
+        hare = head
+        
+        while hare != None:
             turtle = turtle.next
-            rabbit = rabbit.next
-            if rabbit == None:
+            hare = hare.next
+            if hare == None:
                 return False
-            rabbit = rabbit.next
-            if rabbit == None:
-                return False
-                
-            if turtle == rabbit:
+            hare = hare.next
+
+            if turtle == hare:
                 return True
+        return False
+
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.next = None
+
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         if head == None:
+#             return False
+#         turtle = head
+#         rabbit = head
+#         while True:
+#             turtle = turtle.next
+#             rabbit = rabbit.next
+#             if rabbit == None:
+#                 return False
+#             rabbit = rabbit.next
+#             if rabbit == None:
+#                 return False
+                
+#             if turtle == rabbit:
+#                 return True
