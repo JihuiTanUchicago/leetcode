fa0130af71cd4b687c88b1f2e4fa8ba5524cbdc5
JihuiTanUchicago <tanjihui@uchicago.edu>
2026-01-06 15:04:33 -0500

    704

--- python/48. Rotate Image.py
+++ python/48. Rotate Image.py
@@ -3,9 +3,27 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        print(matrix)
-        for i in range(len(matrix)):
-            for j in range(i, len(matrix)):
+        n = len(matrix)
+        for col in range(n):
+            start, end = 0, n-1
+            while start < end:
+                matrix[start][col], matrix[end][col] = matrix[end][col], matrix[start][col]
+                start += 1
+                end -= 1
+        
+        for i in range(n):
+            for j in range(i+1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        for i in range(len(matrix)):
-            matrix[i].reverse()
+
+        
+# class Solution:
+#     def rotate(self, matrix: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+#         print(matrix)
+#         for i in range(len(matrix)):
+#             for j in range(i, len(matrix)):
+#                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+#         for i in range(len(matrix)):
+#             matrix[i].reverse()
--- python/704. Binary Search.py
+++ python/704. Binary Search.py
@@ -1,21 +1,34 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
+        left, right = 0, len(nums)-1
         while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] > target:
-                right = mid - 1
+            mid = (right + left) // 2
+            val = nums[mid]
+            if val > target:
+                right -= 1
+            elif val < target:
+                left += 1
             else:
-                left = mid + 1
+                return mid
         return -1
-
-# import bisect
 # class Solution:
 #     def search(self, nums: List[int], target: int) -> int:
-#         i = bisect.bisect_left(nums, target)
-#         if 0 <= i < len(nums) and nums[i] == target:
-#             return i
+#         left = 0
+#         right = len(nums) - 1
+#         while left <= right:
+#             mid = (left + right) // 2
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid] > target:
+#                 right = mid - 1
+#             else:
+#                 left = mid + 1
 #         return -1
+
+# # import bisect
+# # class Solution:
+# #     def search(self, nums: List[int], target: int) -> int:
+# #         i = bisect.bisect_left(nums, target)
+# #         if 0 <= i < len(nums) and nums[i] == target:
+# #             return i
+# #         return -1
