87afbe7407e49ff0aab5b1e6c300d09fdaa1f250
JihuiTanUchicago <tanjihui@uchicago.edu>
2026-01-06 16:34:26 -0500

    347

--- python/347. Top K Frequent Elements.py
+++ python/347. Top K Frequent Elements.py
@@ -1,6 +1,24 @@
-from collections import Counter
+import heapq
+from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = Counter(nums)
-        most_commons = counter.most_common(k)
-        return [l[0] for l in most_commons]
+        heap = []
+        count_dict = defaultdict(int)
+        for num in nums:
+            count_dict[num] += 1
+        for num in count_dict:
+            heapq.heappush(heap, (-count_dict[num], num))
+        
+        output = []
+        while k != 0:
+            k -= 1
+            output.append(heapq.heappop(heap)[1])
+        
+        return output
+
+# from collections import Counter
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         counter = Counter(nums)
+#         most_commons = counter.most_common(k)
+#         return [l[0] for l in most_commons]
