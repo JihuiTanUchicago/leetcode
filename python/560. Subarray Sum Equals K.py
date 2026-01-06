c25024112f09becce2755a147e34bfb5e588ca61
JihuiTanUchicago <tanjihui@uchicago.edu>
2026-01-06 16:09:22 -0500

    560

--- python/560. Subarray Sum Equals K.py
+++ python/560. Subarray Sum Equals K.py
@@ -2,11 +2,25 @@ from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         count = 0
-        cum_sum = 0
-        sum_dict = defaultdict(int)
-        sum_dict[0] = 1
-        for i in range(len(nums)):
-            cum_sum += nums[i]
-            count += sum_dict[cum_sum-k]
-            sum_dict[cum_sum] += 1
+        sum_count = defaultdict(int)
+        sum_count[0] = 1
+        summation = 0
+        for num in nums:
+            summation += num
+            count += sum_count[summation - k]
+            sum_count[summation] += 1
+        
         return count
+
+# from collections import defaultdict
+# class Solution:
+#     def subarraySum(self, nums: List[int], k: int) -> int:
+#         count = 0
+#         cum_sum = 0
+#         sum_dict = defaultdict(int)
+#         sum_dict[0] = 1
+#         for i in range(len(nums)):
+#             cum_sum += nums[i]
+#             count += sum_dict[cum_sum-k]
+#             sum_dict[cum_sum] += 1
+#         return count
